751644a29bc18ab937b68db837007380cd562631
MarahIrshaid <marah.irshaid96@gmail.com>
2020-06-23 17:32:04 +0300

    added js file

---
+++ js/lab06.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var iDNumber = prompt('please enter your ID number');
+var Name = prompt('please enter your full name');
+var gender = prompt('please enter your gender');
+var age = prompt('please enter your age ');//
+
+
+var greeting;//undefined
+console.log('greeting is ',greeting);
+
+if ( age >= 18 && age <= 99) {
+  greeting = 'Welcome to our website';
+} else if (age > 12 && age < 18) {
+  greeting = 'please have a parent with during using our website!';
+} else {
+  greeting = 'Sorry! you cannot use our website because you are underaged';
+}
+console.log('new value greeting is ',greeting);
+// true / false
+// if(condition){
+// }
+document.write('<h3>' + greeting + '</h3>');
